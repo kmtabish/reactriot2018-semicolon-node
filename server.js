b32c7f34a770bb1a30f439200be5f86d098adec7
tabish.khan <tabish.khan@tothenew.com>
2018-09-15 19:50:38 +0530

    added user route

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
 require('./app/routes/routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000), () => {
     console.log("Server is listening on port 3000");
 });
